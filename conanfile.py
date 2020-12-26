ef34bbf42cbbc58253d4d0097868cd623e16bd21
Agnieszka <agnieszkalasota1994@gmail.com>
2020-12-26 15:16:17 +0100

    :arrow_up: add conan to the project

---
+++ conanfile.py
@@ -0,0 +1,10 @@
+from conans import ConanFile
+
+class ConanPackage(ConanFile):
+    name = 'network-monitor'
+    version = "0.1.0"
+
+    generators = 'cmake_find_package'
+
+    requires = [
+    ]
