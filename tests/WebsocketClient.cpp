b02bf494d612e50975f3da9c7a3750dacbd8c2e2
Agnieszka <agnieszkalasota1994@gmail.com>
2021-02-07 18:15:24 +0100

    Add support for the Boost unit test framework

--- tests/WebsocketClient.cpp
+++ tests/WebsocketClient.cpp
@@ -1,13 +1,15 @@
-#include "network-monitor/WebsocketClient.h"
+#include <network-monitor/WebsocketClient.h>
 
 #include <boost/asio.hpp>
+#include <boost/test/unit_test.hpp>
 
-#include <iostream>
 #include <string>
 
 using NetworkMonitor::WebsocketClient;
 
-int main()
+BOOST_AUTO_TEST_SUITE(network_monitor);
+
+BOOST_AUTO_TEST_CASE(class_WebsocketClient)
 {
     // Connection targets
     const std::string url {"echo.websocket.org"};
@@ -25,8 +27,8 @@ int main()
     bool connected {false};
     bool messageSent {false};
     bool messageReceived {false};
-    bool messageMatches {false};
     bool disconnected {false};
+    std::string echo {};
 
     // Our own callbacks
     auto onSend {[&messageSent](auto ec) {
@@ -41,33 +43,103 @@ int main()
     auto onClose {[&disconnected](auto ec) {
         disconnected = !ec;
     }};
+
+    // The onReceive lambda has been changed to save the received message into echo
     auto onReceive {[&client,
                       &onClose,
                       &messageReceived,
-                      &messageMatches,
-                      &message](auto ec, auto received) {
+                      &echo](auto ec, auto received) {
         messageReceived = !ec;
-        messageMatches = message == received;
+        echo = std::move(received);
         client.Close(onClose);
     }};
 
-    // We must call io_context::run for asynchronous callbacks to run.
+    // call io_context::run for asynchronous callbacks to run
     client.Connect(onConnect, onReceive);
     ioc.run();
 
-    // When we get here, the io_context::run function has run out of work to do.
-    bool ok {
-        connected &&
-        messageSent &&
-        messageReceived &&
-        messageMatches &&
-        disconnected
-    };
-    if (ok) {
-        std::cout << "OK" << std::endl;
-        return 0;
-    } else {
-        std::cerr << "Test failed" << std::endl;
-        return 1;
-    }
+    // the io_context::run function has run out of work to do
+    BOOST_CHECK(connected);
+    BOOST_CHECK(messageSent);
+    BOOST_CHECK(messageReceived);
+    BOOST_CHECK(disconnected);
+    BOOST_CHECK_EQUAL(message, echo);
 }
+
+BOOST_AUTO_TEST_SUITE_END();
+
+
+
+// #include "network-monitor/WebsocketClient.h"
+
+// #include <boost/asio.hpp>
+
+// #include <iostream>
+// #include <string>
+
+// using NetworkMonitor::WebsocketClient;
+
+// int main()
+// {
+//     // Connection targets
+//     const std::string url {"echo.websocket.org"};
+//     const std::string port {"80"};
+//     const std::string message {"Hello Websocket"};
+
+//     // Always start with an I/O context object.
+//     boost::asio::io_context ioc {};
+
+//     // The class under test
+//     WebsocketClient client {url, port, ioc};
+
+//     // We use these flags to check that the connection, send, receive functions
+//     // work as expected.
+//     bool connected {false};
+//     bool messageSent {false};
+//     bool messageReceived {false};
+//     bool messageMatches {false};
+//     bool disconnected {false};
+
+//     // Our own callbacks
+//     auto onSend {[&messageSent](auto ec) {
+//         messageSent = !ec;
+//     }};
+//     auto onConnect {[&client, &connected, &onSend, &message](auto ec) {
+//         connected = !ec;
+//         if (!ec) {
+//             client.Send(message, onSend);
+//         }
+//     }};
+//     auto onClose {[&disconnected](auto ec) {
+//         disconnected = !ec;
+//     }};
+//     auto onReceive {[&client,
+//                       &onClose,
+//                       &messageReceived,
+//                       &messageMatches,
+//                       &message](auto ec, auto received) {
+//         messageReceived = !ec;
+//         messageMatches = message == received;
+//         client.Close(onClose);
+//     }};
+
+//     // We must call io_context::run for asynchronous callbacks to run.
+//     client.Connect(onConnect, onReceive);
+//     ioc.run();
+
+//     // When we get here, the io_context::run function has run out of work to do.
+//     bool ok {
+//         connected &&
+//         messageSent &&
+//         messageReceived &&
+//         messageMatches &&
+//         disconnected
+//     };
+//     if (ok) {
+//         std::cout << "OK" << std::endl;
+//         return 0;
+//     } else {
+//         std::cerr << "Test failed" << std::endl;
+//         return 1;
+//     }
+// }
---
+++ tests/main.cpp
@@ -0,0 +1,3 @@
+#define BOOST_TEST_MODULE network-monitor
+#include <boost/test/unit_test.hpp>
+
