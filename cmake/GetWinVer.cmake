41a10a9aee30083e23460241a8dd9be6eccf590d
Agnieszka <agnieszka.lasota@ubudu.com>
2021-10-31 14:17:30 +0100

    Add filesystem as a dependency

---
+++ cmake/GetWinVer.cmake
@@ -0,0 +1,21 @@
+# Get the Windows version for the _WIN32_WINNT preprocessor symbol.
+# Source: https://stackoverflow.com/a/40217291
+macro(get_win_ver version)
+    if(WIN32)
+        if(CMAKE_SYSTEM_VERSION)
+            set(ver ${CMAKE_SYSTEM_VERSION})
+            string(REGEX MATCH "^([0-9]+).([0-9])" ver ${ver})
+            string(REGEX MATCH "^([0-9]+)" verMajor ${ver})
+            # Check for Windows 10.
+            if("${verMajor}" MATCHES "10")
+                set(verMajor "A")
+                string(REGEX REPLACE "^([0-9]+)" ${verMajor} ver ${ver})
+            endif()
+            # Remove all remaining '.' characters.
+            string(REPLACE "." "" ver ${ver})
+            # Prepend each digit with a zero.
+            string(REGEX REPLACE "([0-9A-Z])" "0\\1" ver ${ver})
+            set(${version} "0x${ver}")
+        endif()
+    endif()
+endmacro()
